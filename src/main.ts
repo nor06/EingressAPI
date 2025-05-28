8bca899f851fb0f5a68a5e5afb09189eaf062d97
nor06 <147531372+nor06@users.noreply.github.com>
2025-05-28 10:17:56 +0800

    trigger github workflow in master and added access fingerprint exception endpoint and update triggfer for azure pipelines

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as os from 'os';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();  // Enable CORS
+  app.setGlobalPrefix('api');
+
+  // Get the network interfaces
+  const networkInterfaces = os.networkInterfaces();
+  let ipAddress: string | undefined;
+
+  // Loop through the network interfaces to find the appropriate IP address
+  for (const netInterface of Object.values(networkInterfaces)) {
+    for (const address of netInterface) {
+      // Check for IPv4 and non-internal addresses
+      if (address.family === 'IPv4' && !address.internal) {
+        ipAddress = address.address; // Get the first found IP address
+        break;
+      }
+    }
+    if (ipAddress) break; // Exit loop if an IP address is found
+  }
+
+  const port = 3000; // Define your port here
+
+  // Log the IP address and port to the console
+  console.log(`Server is running on IP address: ${ipAddress || 'localhost'}, Port: ${port}`);
+
+  // Listen on the dynamic IP address or fallback to localhost if none found
+  await app.listen(port, ipAddress || 'localhost');
+}
+
+bootstrap();
